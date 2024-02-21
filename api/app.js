ed9e268327fc60421013a9515e98e81477990277
Juan <arzzdeveloper04@gmail.com>
2024-02-21 15:21:52 -0600

    thirthCommit
    Funciona ya el cors en el lado host y  local

--- api/app.js
+++ api/app.js
@@ -1,25 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Importar los módulos necesarios
+const express = require('express');
+const homeRoutes = require('./routes/homeRouter.js');
+const cors = require('cors');
 
+// Crear una instancia de la aplicación Express
+const app = express();
+
+// Definir el puerto en el que el servidor escuchará las solicitudes
+const port = process.env.PORT || 5000;
+
+var whitelist = ['http://127.0.0.1:5500', 'https://web.arzz.tech', ];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Middleware para analizar datos de solicitudes entrantes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+
+// Configurar las rutas de la aplicación
+app.use('/api', cors(corsOptions), homeRoutes);
+
+// Ruta para redirigir a GitHub
+app.get('/', (req, res) => {
+  res.redirect('https://github.com/ArZz04');
 });
 
-const homeRoutes = require('./routes/homeRouter.js');
-app.use('/api', homeRoutes);
-
-
-app.get('/', (req, res) => {
-  res.redirect('https://github.com/ArZz04')
-})
-
+// Iniciar el servidor y escuchar en el puerto especificado
 app.listen(port, () => {
-  console.log(`La API esta corriendo en el puerto: ${port}`)
+  console.log(`La API está corriendo en el puerto: ${port}`);
 });
